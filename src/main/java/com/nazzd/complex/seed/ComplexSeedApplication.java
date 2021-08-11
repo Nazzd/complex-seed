e00efbcde1305d10145104f9ed5dc4b7e3798757
nazzd <im.guchen@gmail.com>
2021-08-11 11:15:31 +0800

    feat: 项目初始化

---
+++ src/main/java/com/nazzd/complex/seed/ComplexSeedApplication.java
@@ -0,0 +1,13 @@
+package com.nazzd.complex.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexSeedApplication.class, args);
+    }
+
+}
